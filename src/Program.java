caaa8f2f34dc9f7358ecd7fc1aec57a3433a3b5d
TJS Capita <trevor.smith@capita.com>
2022-11-21 15:53:13 +0000

    Updated the comments in my program file

--- src/Program.java
+++ src/Program.java
@@ -7,14 +7,18 @@ public class Program {
         int age1 = 45;
         Integer age2 = new Integer(45);
 
+        //name1 is a string for Fred
         String name1 = "Fred";
         String name2 = "Fred";
         String name3 = "Barney";
         String name4 = "Fred";
         String name5 = "Fred";
         String name6 = new String("Fred");
+
+        //This line is comparing two strings
         System.out.println(name1.equals(name6));
 
+        //This block of code is providing memory addresses for all variables listed
         System.out.println("The memory for age is: " + VM.current().addressOf(age));
         System.out.println("The memory for age1 is: " + VM.current().addressOf(age1));
         System.out.println("The memory for age2 is: " + VM.current().addressOf(age2));
